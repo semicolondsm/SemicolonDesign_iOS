7a73d0a595e4ff59a7d480d68c4d3f3fd7a90e9c
kimxwan <kimxwan0319@naver.com>
2022-07-31 23:48:02 +0900

    ⚡️ :: SDImageView

---
+++ SemicolonDesign/ImageViewer/Preview/SDImageViewerPreview.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct SDImageViewerPreview: View {
+
+    @State var present = false
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                present = true
+            }, label: {
+                Text("Show ImageViewer")
+            })
+        }
+        .sdImageView(
+            isPresented: self.$present,
+            images: [
+                URL(string: "https://i.pinimg.com/originals/1d/25/28/1d2528b6e70d40a68c1d956cfe375e45.jpg")!,
+                URL(string: "https://fitnish.com/wp-content/uploads/2022/03/82886176_3103409266549936_7191193382985662464_n.jpg")!
+            ]
+        )
+    }
+
+}
