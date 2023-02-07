6d1c0e097864c500865129745d665005bfcf18bd
rlarldud1234 <rlarldud6482@naver.com>
2023-02-07 14:16:07 +0900

    ⚡️ :: Extension

---
+++ SemicolonDesign/Sources/Extension/String.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension String {
+    func toDate(_ format: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+
+        return dateFormatter.date(from: self) ?? Date()
+    }
+}
