c2378022516403254fed6a8ce9d5c2915d5a7e9b
rlarldud1234 <rlarldud6482@naver.com>
2023-03-08 17:36:33 +0900

    ⚡️ :: 버튼 밑에만 가리는게 맞는 것 같아서 수정합니다.

--- SemicolonDesign/Sources/Button/FillButton/FillButton.swift
+++ SemicolonDesign/Sources/Button/FillButton/FillButton.swift
@@ -28,9 +28,9 @@ public struct FillButton: View {
                 .background(isDisabled ? Color.Primary.purple50 : Color.Primary.purple400)
         }
         .cornerRadius(type == .rounded ? 12: 0)
-        .padding([.bottom, .leading, .trailing], type == .rounded ? 16 : 0)
+        .padding(.bottom, type == .rounded ? 16 : 0)
         .background(Color.white)
-        .padding(.top, type == .rounded ? 16: 0)
+        .padding([.top, .leading, .trailing], type == .rounded ? 16: 0)
         .disabled(isDisabled)
     }
 }
