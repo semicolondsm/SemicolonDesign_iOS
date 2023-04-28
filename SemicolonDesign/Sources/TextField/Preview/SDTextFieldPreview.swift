1dd48230b7cc892d0d5f80f8b97a028b7584f877
juyeong525 <juyeongno1@naver.com>
2023-04-29 01:50:31 +0900

    ⚡️ :: SDTextField

--- SemicolonDesign/Sources/TextField/Preview/SDTextFieldPreview.swift
+++ SemicolonDesign/Sources/TextField/Preview/SDTextFieldPreview.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct SDTextFieldPreview: View {
     @State var text: String = ""
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 16) {
 
             SDTextField(
                 title: "필드 레이블",
@@ -45,6 +45,15 @@ struct SDTextFieldPreview: View {
                 help: "도움말 텍스트",
                 text: $text,
                 isDisabled: true
+            )
+            .padding(.horizontal, 18)
+            
+            SDTextField(
+                title: "필드 레이블",
+                placeholder: "플레이스 홀더",
+                help: "도움말 텍스트",
+                isImportant: true,
+                text: $text
             )
             .padding(.horizontal, 18)
         }
