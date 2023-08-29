04901031950b1df68625b55428a12a3c4ae906a1
Byung jin <bjcho1503@naver.com>
2023-08-29 21:17:23 +0900

    ⚡️ :: 이미지 픽커 오류맨트 수정

--- SemicolonDesign/Sources/Photos/SDPhotoPicker/SDPhotoPickerModifier.swift
+++ SemicolonDesign/Sources/Photos/SDPhotoPicker/SDPhotoPickerModifier.swift
@@ -6,39 +6,36 @@ struct SDPhotoPickerModifier: ViewModifier {
 
     var singleSelection: Binding<UIImage>?
     var multipleSelection: Binding<[UIImage]>?
-    var errorAction: (() -> Void)?
 
-    @State var isError: Bool = false
+    @State var isErrorAlertPresented: Bool = false
 
-    init(isPresented: Binding<Bool>, selection: Binding<UIImage>, errorAction: (() -> Void)?) {
+    init(isPresented: Binding<Bool>, selection: Binding<UIImage>) {
         self.isPresented = isPresented
         self.singleSelection = selection
         self.multipleSelection = nil
-        self.errorAction = errorAction
     }
 
-    init(isPresented: Binding<Bool>, selection: Binding<[UIImage]>, errorAction: (() -> Void)?) {
+    init(isPresented: Binding<Bool>, selection: Binding<[UIImage]>) {
         self.isPresented = isPresented
         self.singleSelection = nil
         self.multipleSelection = selection
-        self.errorAction = errorAction
     }
 
     func body(content: Content) -> some View {
         content
             .sheet(isPresented: self.isPresented) {
                 if let singleSelection {
-                    SDPhotoPicker(selection: singleSelection, isErrorAlertPresented: self.$isError)
+                    SDPhotoPicker(selection: singleSelection, isErrorAlertPresented: self.$isErrorAlertPresented)
                         .accentColor(.Primary.purple400)
                         .foregroundColor(.red)
                         .tint(.green)
                 } else if let multipleSelection {
-                    SDPhotoPicker(selection: multipleSelection, isErrorAlertPresented: self.$isError)
+                    SDPhotoPicker(selection: multipleSelection, isErrorAlertPresented: self.$isErrorAlertPresented)
                         .accentColor(.Primary.purple400)
                 }
             }
-            .onChange(of: isError) { isError in
-                if isError { errorAction }
+            .sdOkayAlert(isPresented: self.$isErrorAlertPresented) {
+                SDOkayAlert(title: "문제가 발생하였습니다", message: "형식에 맞지 않는 사진입니다.")
             }
     }
 }
--- SemicolonDesign/Sources/Photos/SDPhotoPicker/View + SDPhotoPicker.swift
+++ SemicolonDesign/Sources/Photos/SDPhotoPicker/View + SDPhotoPicker.swift
@@ -1,18 +1,10 @@
 import SwiftUI
 
 public extension View {
-    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<[UIImage]>, errorAction: (() -> Void)? = nil) -> some View {
-        self.modifier(SDPhotoPickerModifier(
-            isPresented: isPresented,
-            selection: selection,
-            errorAction: errorAction
-        ))
+    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<[UIImage]>) -> some View {
+        self.modifier(SDPhotoPickerModifier(isPresented: isPresented, selection: selection))
     }
-    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<UIImage>, errorAction: (() -> Void)? = nil) -> some View {
-        self.modifier(SDPhotoPickerModifier(
-            isPresented: isPresented,
-            selection: selection,
-            errorAction: errorAction
-        ))
+    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<UIImage>) -> some View {
+        self.modifier(SDPhotoPickerModifier(isPresented: isPresented, selection: selection))
     }
 }
