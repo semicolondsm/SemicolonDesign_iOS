64cdf971d0015fc33ae2699c2533476353238d4b
Byung jin <bjcho1503@naver.com>
2023-08-29 20:05:18 +0900

    ⚡️ :: 이미지 픽커 수정

--- SemicolonDesign/Sources/Photos/SDPhotoPicker/View + SDPhotoPicker.swift
+++ SemicolonDesign/Sources/Photos/SDPhotoPicker/View + SDPhotoPicker.swift
@@ -1,10 +1,18 @@
 import SwiftUI
 
 public extension View {
-    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<[UIImage]>) -> some View {
-        self.modifier(SDPhotoPickerModifier(isPresented: isPresented, selection: selection))
+    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<[UIImage]>, errorAction: (() -> Void)?) -> some View {
+        self.modifier(SDPhotoPickerModifier(
+            isPresented: isPresented,
+            selection: selection,
+            errorAction: errorAction
+        ))
     }
-    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<UIImage>) -> some View {
-        self.modifier(SDPhotoPickerModifier(isPresented: isPresented, selection: selection))
+    func sdPhotoPicker(isPresented: Binding<Bool>, selection: Binding<UIImage>, errorAction: (() -> Void)?) -> some View {
+        self.modifier(SDPhotoPickerModifier(
+            isPresented: isPresented,
+            selection: selection,
+            errorAction: errorAction
+        ))
     }
 }
