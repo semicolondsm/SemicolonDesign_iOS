49051651c96dcca3e158ebfb9531cb5587fd45ba
juyeong525 <juyeongno1@naver.com>
2023-08-30 16:08:14 +0900

    ♻️ :: CalenderCell Ui 수정

--- SemicolonDesign/Sources/Calendar/CalendarCell.swift
+++ SemicolonDesign/Sources/Calendar/CalendarCell.swift
@@ -7,22 +7,27 @@ struct CalendarCell: View {
     let startingSpaces: Int
     let daysInMonth: Int
     let daysInPrevMonth: Int
-
+    
     var body: some View {
-        Text(checkMonthType().dayToString())
-            .sdText(
-                type: .body3,
-                textColor: textColor(type: checkMonthType().monthType)
-            )
-            .frame(width: checkSize(), height: checkSize())
-            .background(backgroundColor())
-            .cornerRadius(18)
-            .overlay(
-                RoundedRectangle(cornerRadius: 18)
-                    .stroke(borderColor(),
-                            lineWidth: 2
-                    )
-            )
+        HStack {
+            Spacer()
+            Text(checkMonthType().dayToString())
+                .sdText(
+                    type: .body3,
+                    textColor: textColor(type: checkMonthType().monthType)
+                )
+            Spacer()
+        }
+        .frame(width: 36)
+        .padding(.vertical, 8)
+        .background(backgroundColor())
+        .cornerRadius(18)
+        .overlay(
+            RoundedRectangle(cornerRadius: 18)
+                .stroke(borderColor(),
+                        lineWidth: 2
+                       )
+        )
     }
     func textColor(type: MonthType) -> Color {
         if Int(date.toString("d")) == count - startingSpaces && Date().toString("yyyy MM") == date.toString("yyyy MM") {
@@ -55,13 +60,13 @@ struct CalendarCell: View {
         .map { Int($0.toString("d")) }
         .contains { $0 == count - startingSpaces } ? .Primary.purple100 : .white
     }
-    func checkSize() -> CGFloat {
-        specialDate.filter {
-            $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("dd") != date.toString("dd")
-        }
-        .map { Int($0.toString("d")) }
-        .contains { $0 == count - startingSpaces } ? 34 : 36
-    }
+//    func checkSize() -> CGFloat {
+//        specialDate.filter {
+//            $0.toString("yyyy MM") == date.toString("yyyy MM") && $0.toString("dd") != date.toString("dd")
+//        }
+//        .map { Int($0.toString("d")) }
+//        .contains { $0 == count - startingSpaces } ? 34 : 36
+//    }
 }
 
 struct CalendarCell_Previews: PreviewProvider {
